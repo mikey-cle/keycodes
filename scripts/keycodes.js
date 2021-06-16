d3432d242c21dded26f40b1d089eb6cf7f2f35d2
Michael cleworth <cpalaunch@gmail.com>
2021-06-16 19:30:59 +0100

    base for key selector

---
+++ scripts/keycodes.js
@@ -0,0 +1,9 @@
+const body = document.getElementsByTagName("body");
+let outPut = document.getElementById("textHere");
+let div = document.getElementsByTagName('div');
+const p = document.getElementsByClassName('p');
+let keyOut = document.addEventListener("keydown", (x) =>{
+let newD = document.createElement('div');
+document.querySelector('p').innerHTML = `<P> You pressed "${x.key}" which has the keycode "${x.keyCode}"`;
+
+})
