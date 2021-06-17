29b9474061123bf82f9e3a4cb5f4a748998c5cda
Michael cleworth <cpalaunch@gmail.com>
2021-06-17 07:47:09 +0100

    change to div

--- scripts/keycodes.js
+++ scripts/keycodes.js
@@ -5,5 +5,4 @@ const p = document.getElementsByClassName('p');
 let keyOut = document.addEventListener("keydown", (x) =>{
 let newD = document.createElement('div');
 document.querySelector('div').innerHTML = `<P> You pressed "${x.key}" which has the keycode "${x.keyCode}"`;
-
 })
